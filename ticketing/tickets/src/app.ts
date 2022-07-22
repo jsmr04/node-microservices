8b4bd8d0024f0f8779505ddb6cf53079c3658db3
Jose S. Marmolejos <jsmr04@gmail.com>
2022-07-21 23:25:34 -0400

    updated ticket package

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,6 +4,11 @@ import "express-async-errors";
 import cookieSession from "cookie-session";
 import { NotFoundError , errorHandler } from "@zemix/common";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+
 const app = express();
 
 app.set('trust proxy', true)
@@ -14,6 +19,11 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 )
+
+app.use(currentUserRouter)
+app.use(signInRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
 
 app.all('*', async ()=>{
     throw new NotFoundError()
